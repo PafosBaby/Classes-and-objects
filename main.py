bfe19594b8cd63e873cd7b523f34d59fd0a5ef0f
aleksey <i@pafosbaby.ru>
2024-12-17 16:02:15 +0700

    Домашняя работа по уроку "Специальные методы классов"

--- main.py
+++ main.py
@@ -11,8 +11,22 @@ class House:
             for floor in range(1, new_floor + 1):
                 print(floor)
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
 
 h1.go_to(5)
 h2.go_to(10)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
